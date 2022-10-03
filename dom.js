48619fc74f152484710d2849491c5c84ce086393
DanijelAdrinek <109511553+DanijelAdrinek@users.noreply.github.com>
2022-10-03 09:30:59 -0700

    improve your code by following these steps

--- dom.js
+++ dom.js
@@ -14,6 +14,11 @@ itemlist.addEventListener("click", removeItem);
 
 filter.addEventListener("keyup", filterItems);
 
+/**
+ * needs better documentation, and the same applies to other functions
+ * 
+ * @param {Event} e
+ */
 function addItem(e) {
   e.preventDefault();
   // Get Input Value
@@ -22,9 +27,13 @@ function addItem(e) {
 
   //Create New li element
 
+  // REPLACE OUTDATED VAR WIHT LET AND CONST WHEN POSSIBLE
   var li = document.createElement("li");
+ 
   //Add class name
   li.className = "list-item";
+
+  // NO CONSOLE-LOGS SHOULD BE PRESENT IN PRODUCTUIN CODE UNLESS NECESSARY
   console.log(li);
 
   // Add text node with input value
@@ -32,7 +41,9 @@ function addItem(e) {
   li.appendChild(document.createTextNode(newItem));
 
   var deleteBtn = document.createElement("button");
-  deleteBtn.className = "close-btn";
+  deleteBtn.className = "close-btn";1
+  
+  // use HTML symbols, if impossibele here, render a whole html element using bacticks, and render a html symbol in the html code in js 
   deleteBtn.appendChild(document.createTextNode("X"));
 
   li.appendChild(deleteBtn);
@@ -48,6 +59,7 @@ function removeItem(e) {
 
 function filterItems(e) {
   var text = e.target.value.toLowerCase();
+  
   console.log(text);
 
   var items = itemlist.getElementsByTagName("li");
@@ -56,11 +68,19 @@ function filterItems(e) {
 
   Array.from(items).forEach(function (item) {
     var itemName = item.firstChild.textContent;
+
+    // VERY IMPORTANT, REMOVE ALL CONSOLE-LOGS THAT ARNT 100% NECESSARY
     console.log(itemName);
+    
     if (itemName.toLowerCase().indexOf(text) != -1) {
       item.style.display = "flex";
     } else {
       item.style.display = "none";
     }
+
+    // better code: const {statementName} = (itemName.toLowerCase().indexOf(text) != -1);
+    // item.style.display = {statementName} ? "flex" : "none";
+
+    // it is a good practice to replace if and switch-case statement's when possible, to learn how to do that, take a look here: https://javascript.plainenglish.io/avoiding-if-statements-in-our-javascript-code-29406677268e
   });
 }
